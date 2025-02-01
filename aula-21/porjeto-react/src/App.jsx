54bc03b60e6815ae46cd1a5a6979ceb311fa8833
miguel1235n <migueldefreitasrebelo@gmail.com>
2025-02-01 11:59:31 -0300

    Add files via upload

---
+++ aula-21/porjeto-react/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./layouts/Footer"
+import Header from "./layouts/Header"
+import Main from "./layouts/Main"
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ aula-21/porjeto-react/src/layouts/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    return(
+
+      <header className="vh-10-bg-dark text-whit">
+      <div>
+        <div>
+            Logo
+        </div>
+        <div>
+            Menu
+        </div>
+        
+      </div>
+      </header>
+        
+    )
+}
+
+export default Header;
